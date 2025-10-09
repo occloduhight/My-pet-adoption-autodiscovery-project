356cc5a88e01482a1ce7d85e3f25c505b68d21e3
occloduhight <chinweodochi@gmail.com>
2025-10-09 17:39:07 +0100

    created vpc, subnets, IAM role

---
+++ vault-jenkins/main.tf
@@ -0,0 +1,152 @@
+provider "aws" {
+  region = "eu-west-3"
+  profile = "default"
+}
+locals {
+    name = "odochi"
+}
+# VPC CREATION
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "$(local.name)-vpc"
+  }
+}
+# INTERNET GATEWAY
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "$(local.name)-igw"
+  }
+}
+# PUBLIC SUBNETS
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "$(local.name)-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "$(local.name)-public-subnet-2"
+  }
+}
+
+# PUBLIC ROUTE TABLE
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "$(local.name)public-route-table"
+  }
+}
+
+# ROUTE TABLE ASSOCIATIONS
+resource "aws_route_table_association" "public_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# # SECURITY GROUP
+# resource "aws_security_group" "public_sg" {
+#   name        = "public-sg"
+#   description = "Allow SSH and HTTP"
+#   vpc_id      = aws_vpc.vpc.id
+
+#   ingress {
+#     description = "Allow SSH"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     description = "Allow HTTP"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "public-sg"
+#   }
+# }
+
+# TLS KEY GENERATION (for SSH)
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "$(local.name)-keypair"
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+# Save private key locally
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.key.private_key_pem
+  filename = "${path.module}/$(local.name)-keypair.pem"
+}
+
+# IAM ROLE AND INSTANCE PROFILE
+# Create IAM Role for EC2 with SSM and Admin permissions
+resource "aws_iam_role" "ec2_role" {
+  name = "jenkins-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+# Attach AmazonSSMManagedInstanceCore policy
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Attach AdministratorAccess policy
+resource "aws_iam_role_policy_attachment" "admin_access" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Create Instance Profile
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "jenkins-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
