75b696911286ad77e385130eaa117c10c51ac62e
occloduhight <chinweodochi@gmail.com>
2025-10-18 19:49:40 +0100

    code review

--- main.tf
+++ main.tf
@@ -7,12 +7,40 @@ data "aws_route53_zone" "zone" {
   private_zone = false
 
 }
-#calling acm certificate
-data "aws_acm_certificate" "cert" {
-  domain      = var.domain_name
-  types       = ["AMAZON_ISSUED"]
-  most_recent = true
+# #calling acm certificate
+# data "aws_acm_certificate" "cert" {
+#   domain      = "odochidevops.space"
+#   statuses        = ["ISSUED"]
+#   most_recent = true
+# }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "odochidevops.space"
+  validation_method = "DNS"
 }
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.zone.id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+
 
 # # Fetch existing public Route53 hosted zone
 # data "aws_route53_zone" "zone" {
@@ -79,8 +107,9 @@ module "nexus" {
   vpc            = module.vpc.vpc_id
   keypair        = module.vpc.public_key
   subnet_id      = module.vpc.pub_sub1_id
-  subnets        = module.vpc.pub_sub1_id
-  certificate    = data.aws_acm_certificate.cert.arn
+  # subnets        = module.vpc.pub_sub_ids
+   subnets    = [module.vpc.pub_sub1_id, module.vpc.pub_sub2_id]
+   certificate    = aws_acm_certificate_validation.cert_validation.certificate_arn
   hosted_zone_id = data.aws_route53_zone.zone.id
   domain_name    = var.domain_name
 }
