0a24e45117c34aaf3ab5d24a4a7c2ef7724d44f4
occloduhight <chinweodochi@gmail.com>
2025-10-20 09:04:03 +0100

    code review

--- vault-jenkins/output.tf
+++ vault-jenkins/output.tf
@@ -1,7 +1,19 @@
 output "jenkins_public_ip" {
-  value = aws_instance.jenkins_server.public_ip
+  description = "Public IP address of the Jenkins EC2 instance"
+  value       = aws_instance.jenkins_server.public_ip
 }
 
 output "vault_public_ip" {
-  value = aws_instance.vault.public_ip
+  description = "Public IP address of the Vault EC2 instance"
+  value       = aws_instance.vault.public_ip
+}
+
+output "certificate_arn" {
+  description = "ARN of the validated ACM certificate for the domain"
+  value       = aws_acm_certificate_validation.cert_validation.certificate_arn
+}
+
+output "hosted_zone_id" {
+  description = "ID of the Route53 hosted zone associated with the domain"
+  value       = data.aws_route53_zone.zone.id
 }
