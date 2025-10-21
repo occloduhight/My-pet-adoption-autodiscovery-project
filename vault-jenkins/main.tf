b78bebb3b14612d92016705fc39a679af1df45fa
occloduhight <chinweodochi@gmail.com>
2025-10-22 00:49:13 +0100

    nexus module

--- vault-jenkins/main.tf
+++ vault-jenkins/main.tf
@@ -1,6 +1,7 @@
 locals {
-    name = "odochi"
+  name = "odochi"
 }
+
 # VPC CREATION
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
@@ -10,6 +11,7 @@ resource "aws_vpc" "vpc" {
     Name = "${local.name}-vpc"
   }
 }
+
 # INTERNET GATEWAY
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
@@ -17,6 +19,7 @@ resource "aws_internet_gateway" "igw" {
     Name = "${local.name}-igw"
   }
 }
+
 # create public subnet 1
 resource "aws_subnet" "pub_sub" {
   vpc_id            = aws_vpc.vpc.id
@@ -27,6 +30,7 @@ resource "aws_subnet" "pub_sub" {
     Name = "${local.name}-pub_sub"
   }
 }
+
 # PUBLIC ROUTE TABLE
 resource "aws_route_table" "pub_rt" {
   vpc_id = aws_vpc.vpc.id
@@ -45,30 +49,23 @@ resource "aws_route_table_association" "public_association_1" {
   route_table_id = aws_route_table.pub_rt.id
 }
 
-# resource "aws_route_table_association" "public_association_2" {
-#   subnet_id      = aws_subnet.pub_sub.id
-#   route_table_id = aws_route_table.pub_rt.id
-# }
-# Generate a new RSA private key (for SSH)
+# SSH Key
 resource "tls_private_key" "key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# Create an AWS key pair from the generated public key
 resource "aws_key_pair" "keypair" {
   key_name   = "${local.name}-keypair"
   public_key = tls_private_key.key.public_key_openssh
 }
 
-# Save the private key locally so you can SSH into the instance
 resource "local_file" "private_key_pem" {
   content  = tls_private_key.key.private_key_pem
   filename = "${path.module}/${local.name}-keypair.pem"
 }
 
-# IAM ROLE AND INSTANCE PROFILE
-# Create IAM Role for EC2 with SSM and Admin permissions
+# IAM ROLE & INSTANCE PROFILE
 resource "aws_iam_role" "jenkins_ec2_role" {
   name = "${local.name}-jenkins-ec2-role"
 
@@ -83,26 +80,23 @@ resource "aws_iam_role" "jenkins_ec2_role" {
     }]
   })
 }
+
 resource "aws_iam_instance_profile" "jenkins_instance_profile" {
   name = "jenkins-ec2-profile"
   role = aws_iam_role.jenkins_ec2_role.name
 }
-# data "aws_iam_instance_profile" "jenkins_instance_profile" {
-#   name = "jenkins-ec2-profile"
-# }
-# Attach AmazonSSMManagedInstanceCore policy
+
 resource "aws_iam_role_policy_attachment" "ssm_core" {
- role       = aws_iam_role.jenkins_ec2_role.name
+  role       = aws_iam_role.jenkins_ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# Attach AdministratorAccess policy
 resource "aws_iam_role_policy_attachment" "admin_access" {
   role       = aws_iam_role.jenkins_ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-# Security Group for Jenkins
+# Security Groups
 resource "aws_security_group" "jenkins_sg" {
   name        = "jenkins-sg"
   description = "Allow HTTP, SSH and Jenkins access"
@@ -116,16 +110,18 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-   egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    }
+  }
+
   tags = {
     Name = "${local.name}-jenkins-sg"
   }
 }
+
 # Data Source - Red Hat AMI
 data "aws_ami" "redhat" {
   most_recent = true
@@ -142,52 +138,10 @@ data "aws_ami" "redhat" {
 
   owners = ["309956199498"] # Red Hat official AWS account ID
 }
-# # Jenkins EC2 Instance
-# resource "aws_instance" "jenkins_server" {
-#   ami                         = data.aws_ami.redhat.id # redhat in eu-west-3)
-#   instance_type               = "t3.medium"
-#   key_name               = aws_key_pair.keypair.key_name   
-#    subnet_id                   = aws_subnet.pub_sub.id   
-#   vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
-#   iam_instance_profile = aws_iam_instance_profile.jenkins_instance_profile.name
-#   associate_public_ip_address = true
-
-# root_block_device {
-#     volume_size = 20    
-#     volume_type = "gp3" 
-#     encrypted   = true 
-#   }
-#     user_data = templatefile("./jenkins_userdata.sh", {
-#     region          = var.region
-#     nr_key          = var.nr_key
-#     nr_acc_id       = var.nr_acc_id
-#     RELEASE_VERSION = "8"
-#   })
-
-# #   user_data = base64encode(templatefile("${path.module}/userdata.sh", {
-# #   region           = var.region,
-# #   newrelic_license = var.newrelic_license,
-# #   nexus_ip         = module.nexus.nexus_ip
-# # }))
-
-# #     user_data = templatefile("./jenkins_userdata.sh", {
-# #     region     = var.region
-# #     nr_key     = var.nr_key
-# #     nr_acc_id  = var.nr_acc_id
-# # })
-#   metadata_options {
-#     http_tokens = "required"
-
-#   }
-
-# tags = {
-#     Name = "${local.name}-Jenkins-Server"
-#   }
-# }
 
 # Jenkins EC2 Instance
 resource "aws_instance" "jenkins_server" {
-  ami                         = data.aws_ami.redhat.id  # Red Hat AMI in eu-west-3
+  ami                         = data.aws_ami.redhat.id
   instance_type               = "t3.medium"
   key_name                    = aws_key_pair.keypair.key_name
   subnet_id                   = aws_subnet.pub_sub.id
@@ -217,259 +171,155 @@ resource "aws_instance" "jenkins_server" {
   }
 }
 
-# Create ACM certificate with DNS validation
-resource "aws_acm_certificate" "acm_cert" {
-  domain_name               = var.domain
-  subject_alternative_names = ["*.${var.domain}"]
-  validation_method         = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name = "${local.name}-acm_cert"
-  }
-}
-data "aws_route53_zone" "acp_zone" {
-  name         = var.domain
-  private_zone = false
-}
-
-# Fetch DNS Validation Records for ACM Certificate
-resource "aws_route53_record" "acm_validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.acm_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-  # Create DNS Validation Record for ACM Certificate
-  zone_id         = data.aws_route53_zone.acp_zone.zone_id
-  allow_overwrite = true
-  name            = each.value.name
-  type            = each.value.type
-  ttl             = 60
-  records         = [each.value.record]
-  depends_on      = [aws_acm_certificate.acm_cert]
-}
-# Validate the ACM Certificate after DNS Record Creation
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.acm_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.acm_validation_record : record.fqdn]
-  depends_on              = [aws_acm_certificate.acm_cert]
-}
-
-# Create Security group for the jenkins elb
-resource "aws_security_group" "jenkins_elb_sg" {
-  name        = "${local.name}-jenkins-elb-sg"
-  description = "Allow HTTPS"
-  vpc_id      = aws_vpc.vpc.id
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "${local.name}-jenkins-elb-sg"
-  }
-}
-
-# Create elastic Load Balancer for Jenkins
-resource "aws_elb" "elb_jenkins" {
-  name            = "elb-jenkins"
-  security_groups = [aws_security_group.jenkins_elb_sg.id] 
-  subnets         = [ aws_subnet.pub_sub.id ]
-
-  listener {
-    instance_port      = 8080
-    instance_protocol  = "HTTP"
-    lb_port            = 443
-    lb_protocol        = "HTTPS"
-    ssl_certificate_id = aws_acm_certificate.acm_cert.arn
-  }
-  health_check {
-    healthy_threshold   = 3
-    unhealthy_threshold = 2
-    interval            = 30
-    timeout             = 5
-    target              = "TCP:8080"
-  }
-  instances                   = [aws_instance.jenkins_server.id]
-  cross_zone_load_balancing   = true
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
-  tags = {
-    Name = "${local.name}-jenkins-server"
-  }
-}
-
-# Data source to get the latest Ubuntu AMI
+# DATA SOURCES
+# Fetch the most recent Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
 }
-# # Security Group for Vault
-# resource "aws_security_group" "vault_sg" {
-#   name        = "vault-sg"
-#   description = "Allow Vault and SSH traffic"
-#   vpc_id      = aws_vpc.vpc.id
-#   # Inbound: HTTP on port 
-#   ingress {
-#     from_port   = 8200
-#     to_port     = 8200
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#     description = "Vault UI/API access"
-#   }
-#  # Outbound: Allow all traffic (to EC2)
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-# create a vault server
-resource "aws_instance" "vault_server" {
-  ami                         = data.aws_ami.ubuntu.id           
-  instance_type               = "t2.medium"                      
-  subnet_id                   =    aws_subnet.pub_sub.id     
-  vpc_security_group_ids      = [aws_security_group.vault_sg.id] 
-  key_name               = aws_key_pair.keypair.key_name
-  associate_public_ip_address = true                             
-  iam_instance_profile = aws_iam_instance_profile.vault_ssm_profile.name
-  root_block_device {  
-    volume_size = 20
-    volume_type = "gp3"
-    encrypted   = true
-  }
-  # User data script to install Jenkins and required tools
-  user_data = templatefile("./vault.sh", {
-    region        = var.region,
-    VAULT_VERSION = "1.18.3",
-    key           = aws_kms_key.vault.id
-  })
-  metadata_options {
-    http_tokens = "required"
-  }
-  # Tag the instance for easy identification
-  tags = {
-    Name = "${local.name}-vault-server"
-  }
+
+# Fetch Route53 hosted zone
+data "aws_route53_zone" "selected" {
+  name         = "odochidevops.space."   # replace with your domain
+  private_zone = false
 }
-# create KMS key to manage vault unseal keys
-resource "aws_kms_key" "vault" {
-  description             = "An example symmetric encryption KMS key"
-  enable_key_rotation     = true
-  deletion_window_in_days = 20
-  tags = {
-    Name = "${local.name}-vault-kms-key"
-  }
-}
-# Security Group for ELB to allow HTTP traffic
+
+# Security group for Vault server
 resource "aws_security_group" "vault_sg" {
   name        = "${local.name}-vault-sg"
-  description = "Allow HTTP traffic to server"
-  vpc_id      = aws_vpc.vpc.id
-  # Inbound: HTTP on port 80
+  description = "Allow SSH, HTTP, HTTPS, and Vault UI/API"
+  vpc_id      = aws_vpc.vpc.id  # ensure aws_vpc.main exists or update to your actual VPC reference
+
   ingress {
+    description = "Vault HTTP"
     from_port   = 8200
     to_port     = 8200
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # Outbound: Allow all traffic (to EC2)
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "${local.name}-vault-sg"
   }
 }
 
-#creating and attaching an IAM role with SSM permissions to the vault instance.
-resource "aws_iam_role" "vault_ssm_role" {
-  name = "${local.name}-ssm-vault-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
+resource "aws_instance" "vault_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.medium"
+  key_name               = aws_key_pair.keypair.key_name
+  subnet_id              = aws_subnet.pub_sub.id
+  vpc_security_group_ids = [aws_security_group.vault_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.vault_profile.name
+  associate_public_ip_address = true
+  tags = {
+    Name = "${local.name}-vault-server"
+  }
 }
-# create iam role policy forto give permission to the kms role
-resource "aws_iam_role_policy" "kms_policy" {
-  name = "${local.name}-kms-policy1"
-  role = aws_iam_role.vault_ssm_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDatakey*",
-          "kms:DescribeKey"
-        ],
-        Effect   = "Allow"
-        Resource = "${aws_kms_key.vault.arn}"
-      }
-    ]
-  })
+# ACM CERTIFICATE + VALIDATION
+resource "aws_acm_certificate" "acm_cert" {
+  domain_name       = "vault.odochidevops.space"
+  validation_method = "DNS"
+  tags = {
+    Name = "vault-acm-cert"
+  }
 }
-#Attach the AmazonSSMManagedInstanceCore policy
-# — required for Session Manager and SSM Agent functionality.
-# resource "aws_iam_role_policy_attachment" "vault_ssm_attachment" {
-#   role       = aws_iam_role.vault_ssm_role.id
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-# }
-# data "aws_iam_instance_profile" "vault_ssm_profile" {
-#   name = "odochi-ssm-vault-instance-profile"
-# }
-# create instance profile for vault
-resource "aws_iam_instance_profile" "vault_ssm_profile" {
-  name = "${local.name}-ssm-vault-instance-profile"
-  role = aws_iam_role.vault_ssm_role.id
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_cert.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
 }
-# Security Group for ELB to allow HTTP traffic
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.acm_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+# ELB FOR VAULT
+resource "aws_elb" "vault_elb" {
+  name               = "${local.name}-vault-elb"
+  subnets            = [aws_subnet.pub_sub.id]
+  security_groups    = [aws_security_group.vault_sg.id]
+
+  listener {
+    instance_port     = 8200
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  instances = [aws_instance.vault_server.id]
+
+  health_check {
+    target              = "HTTP:8200/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${local.name}-vault-elb"
+  }
+}
+# ROUTE53 RECORD FOR VAULT
+resource "aws_route53_record" "vault_record" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "vault.${trim(data.aws_route53_zone.selected.name, ".")}"
+  type    = "A"
+
+  alias {
+    name                   = aws_elb.vault_elb.dns_name
+    zone_id                = aws_elb.vault_elb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+# ELB Security Group vault
 resource "aws_security_group" "vault_elb_sg" {
   name        = "${local.name}-vault-elb-sg"
-  description = "Allow HTTP traffic to server"
+  description = "Allow inbound HTTPS traffic"
   vpc_id      = aws_vpc.vpc.id
-  # Inbound: HTTP on port 80
   ingress {
+    description = "Allow HTTPS traffic"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # Outbound: Allow all traffic (to EC2)
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -479,66 +329,67 @@ resource "aws_security_group" "vault_elb_sg" {
     Name = "${local.name}-vault-elb-sg"
   }
 }
-# Create a new load balancer for vault
-resource "aws_elb" "vault_elb" {
-  name               = "${local.name}-vault-elb"
-  security_groups    = [aws_security_group.vault_elb_sg.id]
-  subnets         = [aws_subnet.pub_sub.id]  # Use first available subnet
-  cross_zone_load_balancing = true
 
- listener {
-    instance_port      = 8200
-    instance_protocol  = "http"
-    lb_port            = 443
-    lb_protocol        = "https"
-    ssl_certificate_id = aws_acm_certificate.acm_cert.arn
-  }
-
-  health_check {
-    target              = "TCP:8200"
-    interval            = 30
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-  instances = [aws_instance.vault_server.id]
-
-  depends_on = [aws_acm_certificate_validation.cert_validation]
+# Vault IAM Role vault
+resource "aws_iam_role" "vault_role" {
+  name = "${local.name}-vault-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
   tags = {
-    Name = "${local.name}-vault-elb"
+    Name = "${local.name}-vault-role"
+  }
+}
+# Attach KMS access policy to Vault IAM Role
+resource "aws_iam_role_policy" "vault_kms_access" {
+  name = "${local.name}-vault-kms-access"
+  role = aws_iam_role.vault_role.id   # ensure this matches your actual IAM role name
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        Resource = aws_kms_key.kms_key.arn
+      }
+    ]
+  })
+}
+
+# Create a KMS key for Vault
+resource "aws_kms_key" "kms_key" {
+  description             = "${local.name}-vault-kms-key"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+
+  tags = {
+    Name = "${local.name}-vault-kms-key"
   }
 }
 
-# Create Route 53 record for vault server
-resource "aws_route53_record" "vault_record" {
-  zone_id = data.aws_route53_zone.acp_zone.id
-  name    = "vault.${var.domain}"
-  type    = "A"
-  alias {
-    name                   = aws_elb.vault_elb.dns_name
-    zone_id                = aws_elb.vault_elb.zone_id
-    evaluate_target_health = true
-  }
+# Create a KMS alias
+resource "aws_kms_alias" "kms_alias" {
+  name          = "alias/${local.name}-kms-key"
+  target_key_id = aws_kms_key.kms_key.key_id
 }
 
-# Create Route 53 record for jenkins server
-resource "aws_route53_record" "jenkins" {
-  zone_id = data.aws_route53_zone.acp_zone.id
-  name    = "jenkins.${var.domain}"
-  type    = "A"
-  alias {
-    name                   = aws_elb.elb_jenkins.dns_name
-    zone_id                = aws_elb.elb_jenkins.zone_id
-    evaluate_target_health = true
-  }
+# Vault IAM Instance Profile
+resource "aws_iam_instance_profile" "vault_profile" {
+  name = "${local.name}-vault-profile"
+  role = aws_iam_role.vault_role.name
 }
-
-# resource "aws_iam_role_policy_attachment" "ssm_core_jenkins" {
-#   role       = aws_iam_role.jenkins_ec2_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-# }
-
-# resource "aws_iam_role_policy_attachment" "admin_access_jenkins" {
-#   role       = aws_iam_role.jenkins_ec2_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-# }
