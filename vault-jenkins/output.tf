a20fbd0f0a3067af6fdd9365bef97af27ea4f696
occloduhight <chinweodochi@gmail.com>
2025-10-22 00:49:13 +0100

    nexus module

--- vault-jenkins/output.tf
+++ vault-jenkins/output.tf
@@ -1,19 +1,12 @@
 output "jenkins_public_ip" {
   value = aws_instance.jenkins_server.public_ip
 }
-
 output "vault_public_ip" {
   value = aws_instance.vault_server.public_ip
 }
 
-output "jenkins_elb_dns" {
-  value = aws_elb.elb_jenkins.dns_name
+
+output "jenkins_instance_id" {
+  value = aws_instance.jenkins_server.id
 }
 
-output "vault_elb_dns" {
-  value = aws_elb.vault_elb.dns_name
-}
-
-output "acm_certificate_arn" {
-  value = aws_acm_certificate.acm_cert.arn
-}
