822538d26698b57f6f7b66b5b45d8335a23272b1
occloduhight <chinweodochi@gmail.com>
2025-10-22 00:51:37 +0100

    nexus module

--- variable.tf
+++ variable.tf
@@ -1,23 +1,71 @@
-variable "domain" {}
-variable "nr_key" {}
-variable "nr_acc_id" {}
-variable "region" {}
-  
+# variable "domain" {}
+# variable "nr_key" {}
+# variable "nr_acc_id" {}
+# variable "region" {} 
+# variable "name" {}
+# variable "az1" {}
+# variable "az2" {}
+# variable "keypair" {}
+# variable "privatekey {}
+#  variable "certificate" {}
+# # variable "hosted_zone_id" {}
+ 
+ variable "domain" {
+  description = "Root domain for Route53 records"
+  type        = string
+}
+
+variable "nr_key" {
+  description = "New Relic license key"
+  type        = string
+}
+
+variable "nr_acc_id" {
+  description = "New Relic account ID"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-3"
+}
+
 variable "name" {
   description = "Project name prefix"
   type        = string
-  default     = "odochi"
 }
 
-
 variable "az1" {
-  description = "Availability Zone 1"
+  description = "Availability zone 1"
   type        = string
-  default     = "eu-west-3a"
 }
 
 variable "az2" {
-  description = "Availability Zone 2"
+  description = "Availability zone 2"
   type        = string
-  default     = "eu-west-3b"
 }
+
+variable "keypair" {
+  description = "Name of EC2 key pair"
+  type        = string
+}
+
+# variable "jenkins_instance_id" {
+#   type = string
+# }
+
+# variable "privatekey" {
+#   description = "Path to private key (.pem)"
+#   type        = string
+# }
+
+# variable "certificate_arn" {
+#   description = "ACM certificate ARN for HTTPS"
+#   type        = string
+# }
+
+# variable "hosted_zone_id" {
+#   description = "Route53 Hosted Zone ID"
+#   type        = string
+# }
