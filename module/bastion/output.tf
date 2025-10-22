643ea1ea9c9f73352bc350467807fa501898b98d
occloduhight <chinweodochi@gmail.com>
2025-10-22 12:39:16 +0100

    reviewed ansible module

--- module/bastion/output.tf
+++ module/bastion/output.tf
@@ -3,12 +3,17 @@ output "bastion_asg_name" {
   value       = aws_autoscaling_group.bastion_asg.name
 }
 
-output "bastion_sg_id" {
-  description = "Bastion Security Group ID"
-  value       = aws_security_group.bastion_sg.id
-}
+# output "bastion_sg_id" {
+#   description = "Bastion Security Group ID"
+#   value       = aws_security_group.bastion_sg.id
+# }
 
 output "bastion_iam_instance_profile" {
   description = "Bastion IAM instance profile name"
   value       = aws_iam_instance_profile.bastion_ssm_profile.name
 }
+output "bastion_sg" {
+  description = "Security group ID of the bastion host"
+  value       = aws_security_group.bastion_sg.id
+}
+
---
+++ module/sonarqube/variable.tf
@@ -0,0 +1,11 @@
+variable "key" {}
+variable "name" {}
+variable "subnet_id" {}
+variable "bastion_sg" {}
+variable "subnets" {}
+variable "vpc_id" {}
+variable "domain" {}
+variable "acm_certificate_arn" {}
+variable "nr_key" {}
+variable "nr_id" {}
+
