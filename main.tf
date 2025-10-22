83bb5e961a5d951a07dde1fc38a6acc66cb1cef8
occloduhight <chinweodochi@gmail.com>
2025-10-22 12:39:16 +0100

    reviewed ansible module

--- main.tf
+++ main.tf
@@ -1,8 +1,136 @@
+# locals {
+#   name = "odochi"
+# }
+
+# # Import Jenkins remote state
+# data "terraform_remote_state" "vault" {
+#   backend = "s3"
+#   config = {
+#     bucket = "auto-discovery-odo2025"
+#     key    = "vault-jenkins/terraform.tfstate"
+#     region = "eu-west-3"
+#   }
+# }
+# # VPC Module
+# module "vpc" {
+#   source = "./module/vpc"
+
+#   name   = local.name
+#   region = var.region
+#   az1    = var.az1
+#   az2    = var.az2
+# }
+
+# # ACM Certificate (root module)
+# resource "aws_acm_certificate" "acm_cert" {
+#   domain_name               = var.domain
+#   subject_alternative_names = ["*.${var.domain}"]
+#   validation_method         = "DNS"
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+
+#   tags = {
+#     Name = "${local.name}-acm-cert"
+#   }
+# }
+
+# # Lookup Route53 zone
+# data "aws_route53_zone" "zone" {
+#   name         = var.domain
+#   private_zone = false
+# }
+
+# # Create DNS validation records
+# resource "aws_route53_record" "acm_validation_record" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.acm_cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
+
+#   zone_id         = data.aws_route53_zone.zone.zone_id
+#   allow_overwrite = true
+#   name            = each.value.name
+#   type            = each.value.type
+#   ttl             = 60
+#   records         = [each.value.record]
+
+#   depends_on = [aws_acm_certificate.acm_cert]
+# }
+
+# # Validate ACM certificate
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   certificate_arn         = aws_acm_certificate.acm_cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.acm_validation_record : record.fqdn]
+
+#   depends_on = [aws_route53_record.acm_validation_record]
+# }
+
+# # Bastion Module
+# module "bastion" {
+#   source     = "./module/bastion"
+#   name       = local.name
+#   vpc        = module.vpc.vpc_id
+#   subnets    = [module.vpc.pub_sub1_id, module.vpc.pub_sub2_id]
+#   keypair    = module.vpc.public_key
+#   privatekey = module.vpc.private_key
+#   nr_key     = var.nr_key
+#   nr_acc_id  = var.nr_acc_id
+#   region     = var.region
+# }
+
+# # Nexus Module
+# module "nexus" {
+#   source = "./module/nexus"
+
+#   name        = local.name
+#   vpc         = module.vpc.vpc_id
+#   keypair     = module.vpc.public_key
+#   subnet_id   = module.vpc.pub_sub2_id
+#   subnets     = [module.vpc.pub_sub1_id, module.vpc.pub_sub2_id]
+#   certificate = aws_acm_certificate_validation.cert_validation.certificate_arn
+#   domain      = var.domain
+
+#   # 👇 Automatically fetch Jenkins instance ID from remote state
+#   jenkins_instance_id = data.terraform_remote_state.vault.outputs.jenkins_instance_id
+# }
+
+# module "ansible" {
+#   source = "./module/ansible"
+#   name = local.name
+#   # bastion_sg = module.bastion.bastion_sg
+#   private_key = module.vpc.private_key
+#   keypair = module.vpc.public_key
+#   subnet_id = module.vpc.pri_sub1_id
+#   nexus_ip = ""
+#   vpc = module.vpc.vpc_id
+#   nr_acc_id = var.nr_acc_id
+#   nr_key = var.nr_key
+#   bastion_sg = module.bastion.bastion_sg
+
+# }
+# # module "ansible" {
+# #   source = "./module/ansible"  # path to your ansible module
+
+# #   name             = var.name
+# #   vpc              = var.vpc_id
+# #   subnet_id        = var.subnet_id
+# #   bastion_sg       = var.bastion_sg
+# #   keypair          = var.keypair
+# #   ansible_userdata = var.ansible_userdata
+# # }
+
 locals {
   name = "odochi"
 }
 
+# ---------------------------
 # Import Jenkins remote state
+# ---------------------------
 data "terraform_remote_state" "vault" {
   backend = "s3"
   config = {
@@ -11,7 +139,10 @@ data "terraform_remote_state" "vault" {
     region = "eu-west-3"
   }
 }
+
+# ---------------------------
 # VPC Module
+# ---------------------------
 module "vpc" {
   source = "./module/vpc"
 
@@ -21,7 +152,9 @@ module "vpc" {
   az2    = var.az2
 }
 
+# ---------------------------
 # ACM Certificate (root module)
+# ---------------------------
 resource "aws_acm_certificate" "acm_cert" {
   domain_name               = var.domain
   subject_alternative_names = ["*.${var.domain}"]
@@ -36,13 +169,11 @@ resource "aws_acm_certificate" "acm_cert" {
   }
 }
 
-# Lookup Route53 zone
 data "aws_route53_zone" "zone" {
   name         = var.domain
   private_zone = false
 }
 
-# Create DNS validation records
 resource "aws_route53_record" "acm_validation_record" {
   for_each = {
     for dvo in aws_acm_certificate.acm_cert.domain_validation_options : dvo.domain_name => {
@@ -62,7 +193,6 @@ resource "aws_route53_record" "acm_validation_record" {
   depends_on = [aws_acm_certificate.acm_cert]
 }
 
-# Validate ACM certificate
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.acm_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.acm_validation_record : record.fqdn]
@@ -70,7 +200,9 @@ resource "aws_acm_certificate_validation" "cert_validation" {
   depends_on = [aws_route53_record.acm_validation_record]
 }
 
+# ---------------------------
 # Bastion Module
+# ---------------------------
 module "bastion" {
   source     = "./module/bastion"
   name       = local.name
@@ -83,7 +215,9 @@ module "bastion" {
   region     = var.region
 }
 
+# ---------------------------
 # Nexus Module
+# ---------------------------
 module "nexus" {
   source = "./module/nexus"
 
@@ -95,6 +229,22 @@ module "nexus" {
   certificate = aws_acm_certificate_validation.cert_validation.certificate_arn
   domain      = var.domain
 
-  # 👇 Automatically fetch Jenkins instance ID from remote state
+  # Fetch Jenkins instance ID from vault-jenkins remote state
   jenkins_instance_id = data.terraform_remote_state.vault.outputs.jenkins_instance_id
+}
+
+# ---------------------------
+# Ansible Module
+# ---------------------------
+module "ansible" {
+  source      = "./module/ansible"
+  name        = local.name
+  private_key = module.vpc.private_key
+  keypair     = module.vpc.public_key
+  subnet_id   = module.vpc.pri_sub1_id
+  nexus_ip    = ""  # Fill if needed
+  vpc         = module.vpc.vpc_id
+  nr_acc_id   = var.nr_acc_id
+  nr_key      = var.nr_key
+  bastion_sg  = module.bastion.bastion_sg
 }
---
+++ module/sonarqube/main.tf
@@ -0,0 +1,133 @@
+# Ubuntu AMI lookup
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Load user data script from file
+resource "aws_instance" "sonarqube_server" {
+  ami                         = data.aws_ami.ubuntu.id # ubuntu in eu-west-2
+  instance_type               = "t2.medium"
+  key_name                    = var.key
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sonarqube_sg.id]
+  user_data = local.userdata
+  root_block_device {
+    volume_size = 20    # Size in GB
+    volume_type = "gp3" # General Purpose SSD (recommended)
+    encrypted   = true  # Enable encryption (best practice)
+  }
+  tags = {
+    Name = "${var.name}-sonarqube-server2"
+  }
+}
+
+# Create sonarqube security group
+resource "aws_security_group" "sonarqube_sg" {
+  name        = "${var.name}-sonarqube-sg"
+  description = "Allow SSH and HTTPS"
+  vpc_id      = var.vpc_id
+  ingress {
+    description = "SSH from Bastion"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [var.bastion_sg]
+  }
+  ingress {
+    description = "HTTP from ELB"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    security_groups = [aws_security_group.elb_sonar_sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.name}-sonarqu-sg"
+  }
+}
+
+#Create Security Group for Sonarqube Sever ELB
+resource "aws_security_group" "elb_sonar_sg" {
+  name        = "${var.name}-sonarqube-elb-sg"
+  description = "Allow HTTPS"
+  vpc_id      = var.vpc_id
+  ingress {
+    description = "HTTPS from Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.name}-sonarqube-elb-sg"
+  }
+}
+
+# Create Elastic load balancer for Sonarqube Server
+resource "aws_elb" "elb_sonar" {
+  name            = "${var.name}-elb-sonarqube"
+  subnets         = var.subnets
+  security_groups = [aws_security_group.elb_sonar_sg.id]
+  instances                   = [aws_instance.sonarqube_server.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+  listener {
+    instance_port      = 9000
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = var.acm_certificate_arn
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:9000"
+    interval            = 30
+  }
+  tags = {
+    Name = "${var.name}-elb-sonarqube"
+  }
+}
+
+# Create Route 53 Hosted Zone for Sonarqube
+data "aws_route53_zone" "zone_id" {
+  name         = var.domain
+  private_zone = false
+}
+
+# Create Route 53 A Record for Sonarqube Server
+resource "aws_route53_record" "sonar_record" {
+  zone_id = data.aws_route53_zone.zone_id.zone_id
+  name    = "sonarqube.${var.domain}"
+  type    = "A"
+  alias {
+    name                   = aws_elb.elb-sonar.dns_name
+    zone_id                = aws_elb.elb-sonar.zone_id
+    evaluate_target_health = true
+  }
+}
