9df15007e1b4565a69cabddc780ed89045613900
occloduhight <chinweodochi@gmail.com>
2026-01-18 22:06:09 +0100

    database module updated

--- module/database/main.tf
+++ module/database/main.tf
@@ -43,13 +43,13 @@ resource "aws_db_instance" "database" {
   engine_version         = "8.0"
   instance_class         = "db.t3.micro"
   parameter_group_name   = "default.mysql8.0"
-  db_name                = "petclinic"
+  db_name                = "myproject"
   username               = var.db_username
   password               = var.db_password
   skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
-  multi_az               = false
+  multi_az               = true
   publicly_accessible    = false
 
   tags = {
