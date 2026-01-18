b20a732ee9da46bf6c04c7279d07b07dbed15c2b
occloduhight <chinweodochi@gmail.com>
2026-01-18 22:02:54 +0100

    bastion module updated

--- module/bastion/output.tf
+++ module/bastion/output.tf
@@ -1,3 +1,4 @@
 output "bastion_sg" {
-  value = aws_security_group.bastion_sg.id
+    value = aws_security_group.bastion_sg.id
+  
 }
--- module/bastion/variable.tf
+++ module/bastion/variable.tf
@@ -1,7 +1,6 @@
-variable "region" {}
 variable "name" {}
 variable "key_name" {}
-variable "subnet" {}
+variable "subnets" {}
 variable "private_key" {}
 variable "nr_key" {}
 variable "nr_acc_id" {}
