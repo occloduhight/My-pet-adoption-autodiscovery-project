66ff166ccd66b3e36f1976ba902a195520f0a83d
occloduhight <chinweodochi@gmail.com>
2026-01-18 22:17:44 +0100

    sonar module updated

---
+++ module/sonar/variable.tf
@@ -0,0 +1,8 @@
+variable "name" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "key_name" {}
+variable "domain_name" {}
+variable "public_subnets" {}
+variable "nr_key" {}
+variable "nr_acc_id" {}
--- module/sonarqube/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "subnet" {}
-variable "name" {}
-variable "key_name" {}
-variable "vpc_id" {}
-variable "nr_key" {}
-variable "nr_acc_id" {}
-variable "subnets_elb" {}
-variable "domain" {}
